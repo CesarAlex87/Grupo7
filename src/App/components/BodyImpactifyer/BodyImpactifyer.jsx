78c74d7d03664d671ca9e0a33be6ee7ad57e1dce
CesarAlex87 <tipanantonc@gmail.com>
2023-07-09 06:32:21 -0500

    ...

--- src/App/components/BodyImpactifyer/BodyImpactifyer.jsx
+++ src/App/components/BodyImpactifyer/BodyImpactifyer.jsx
@@ -12,29 +12,37 @@ const BodyImpactifyer = () => {
       </div>
       <div className="sections">
       <HeadPublifyer className="profileInfo"/>
-        <div className="content-rectangle content-video">
+        <div className="content-rectangle sectionVideos">
           <div className="titleSection">
             <img src={icon[0]} alt=""  className="icon"/>
             <h2> TOP VIDEOS</h2>
-            <div className="video-container">
+          </div>
+          <div className="video-container">
               <div className="video-item">
                 <img  src={icon[2]} alt="Video 1" />
-                </div>
                 <p>Visitas: 300k</p>
                 <p>Comentario mas likeado: ¡AWEEEEEE 🥰🥰🥰!</p>
                 <p>League of Leyends</p>
-                <div className="video-item">
+              </div>
+              <div className="video-item">
                   <img  src={icon[3]} alt="Video 2" />
-                  </div>
                   <p>Visitas: 170k</p>
                   <p>Comentario mas likeado: ¡Me encanta la música en este video! 🎵</p>
                   <p>Music Edit</p>
-                  <div className="video-item">
+              </div>    
+              <div className="video-item">
                     <img  src={icon[4]} alt="Video 3" />
-                    </div>
                     <p>Visitas: 80k</p>
                     <p>Comentario mas likeado: ¡Esto alegró mi día! ❤️</p>
                     <p>Motivation</p>
+              </div> 
+          </div>
+        </div>
+        
+        <div className="content-rectangle sectionComments">
+        <div className="titleSection">
+            <img src={icon[0]} alt="" />
+            <h2>COMENTARIOS</h2>
           </div>
         </div>
         <div className="content-rectangle" >
@@ -43,19 +51,13 @@ const BodyImpactifyer = () => {
             <h2>TENDENCIAS</h2>
           </div>
         </div>
-        <div className="content-rectangle">
-        <div className="titleSection">
-            <img src={icon[0]} alt="" />
-            <h2>COMENTARIOS</h2>
-          </div>
-        </div>
+        
         <div className="content-rectangle">
         <div className="titleSection">
             <img src={icon[0]} alt="" />
             <h2>HERRAMIENTAS</h2>
           </div>
         </div>      
-      </div>
       </div>
       </div>
 
