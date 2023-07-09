7baba400683f31dc2cba26ef579bcd25c2d94b05
CesarAlex87 <101759547+CesarAlex87@users.noreply.github.com>
2023-07-08 23:53:11 -0500

    Update img.js

--- src/App/components/MenuPublifyer/img.js
+++ src/App/components/MenuPublifyer/img.js
@@ -1,4 +1,7 @@
 import logo from "../../../assets/img/image 1.svg";
 import dashboard from "../../../assets/img/dashboard.svg";
+import reporte from "../../../assets/img/reporte.svg";
+import perfil from "../../../assets/img/perfil.svg";
+import impactify from "../../../assets/img/impactify.svg";
 
 export default [logo, dashboard];
