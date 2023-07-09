d03445014913e055ad2c93985d5e28e19a2a8f60
Chrxs <trabajo.ochoachristopher@gmail.com>
2023-07-09 02:38:56 -0500

    VAMOOOOOS

--- src/App/AppUi.jsx
+++ src/App/AppUi.jsx
@@ -3,33 +3,26 @@ import "./App.css";
 import { MenuPublifyer } from "./components/MenuPublifyer/MenuPublifyer";
 import { HeadPublifyer } from "./components/HeadImpactifyer/HeadPublifyer";
 import { BodyImpactifyer } from "./components/BodyImpactifyer/BodyImpactifyer";
-import ChartComponent from './components/ChartComponent/ChartComponent';
-import dataSet from './src/DataSetTendencias.json';
+// import ChartComponent from "./components/ChartComponent/ChartComponent";
+// import dataSet from "./src/DataSetTendencias.js";
 
 const AppUi = () => {
   return (
-<<<<<<< HEAD
     <>
-      <HeadPublifyer />
-      <MenuPublifyer />
-      <BodyImpactifyer />
-      <ChartComponent dataSet={dataSet} />
+      <div className="container">
+        <div className="menu">
+          <MenuPublifyer />
+        </div>
+        <div className="body">
+          <BodyImpactifyer />
+        </div>
+        <div className="head">
+          <HeadPublifyer />
+        </div>
+      </div>
+      {/* <ChartComponent dataSet={dataSet} /> */}
     </>
-=======
-    <div className="container">
-      <div className="menu">
-        <Menu />
-      </div>
-      <div className="body">
-        <Body />
-      </div>
-      <div className="head">
-        <Head />
-      </div>
-    </div>
->>>>>>> ccb1f9c910cfb73ede7f5fba773164f17ea07d32
   );
 };
 
 export { AppUi };
-
