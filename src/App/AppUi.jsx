bd5a8726a5c5eaeb3cd3ee2f6838355e1b1e0c76
Chrxs <trabajo.ochoachristopher@gmail.com>
2023-07-09 02:01:58 -0500

    hola

--- src/App/AppUi.jsx
+++ src/App/AppUi.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import { MenuPublifyer } from "./components/MenuPublifyer/MenuPublifyer";
 import { HeadPublifyer } from "./components/HeadImpactifyer/HeadPublifyer";
 import { BodyImpactifyer } from "./components/BodyImpactifyer/BodyImpactifyer";
-import { Grafica } from "./components/Graph/Graph";
-import jsonData from './DataSetTendencias.json'; 
-
-
+import { Grafica } from "./components/Grafica/Graph.js";
 
 const AppUi = () => {
   return (
@@ -13,8 +10,6 @@ const AppUi = () => {
       <HeadPublifyer />
       <MenuPublifyer />
       <BodyImpactifyer />
-      <ChartComponent dataset={jsonData} />
-
     </>
   );
 };
--- src/App/components/Grafica/Graph.js
+++ src/App/components/Grafica/Graph.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import React from "react";
+import { Bar } from "react-chartjs-2";
 
 const Graph = ({ dataset }) => {
   // Extraer los datos relevantes del objeto JSON
-  const tags = dataset.body.discoverList.filter(item => item.type === 3).map(item => item.title);
-  const music = dataset.body.discoverList.filter(item => item.type === 4).map(item => item.title);
+  const tags = dataset.body.discoverList
+    .filter((item) => item.type === 3)
+    .map((item) => item.title);
+  const music = dataset.body.discoverList
+    .filter((item) => item.type === 4)
+    .map((item) => item.title);
 
   // Crear la configuración del gráfico
   const chartData = {
-    labels: ['Tags', 'Música'],
+    labels: ["Tags", "Música"],
     datasets: [
       {
-        label: 'Frecuencia',
+        label: "Frecuencia",
         data: [tags.length, music.length],
-        backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)'],
-        borderColor: ['rgba(75, 192, 192, 1)', 'rgba(54, 162, 235, 1)'],
+        backgroundColor: ["rgba(75, 192, 192, 0.2)", "rgba(54, 162, 235, 0.2)"],
+        borderColor: ["rgba(75, 192, 192, 1)", "rgba(54, 162, 235, 1)"],
         borderWidth: 1,
       },
     ],
