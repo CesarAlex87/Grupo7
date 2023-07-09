e781149d4b633af96a3b6975fbc87465a236b222
FranBW <milerolarte@gmail.com>
2023-07-09 01:44:50 -0500

    Graph

--- src/App/AppUi.jsx
+++ src/App/AppUi.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { MenuPublifyer } from "./components/MenuPublifyer/MenuPublifyer";
 import { HeadPublifyer } from "./components/HeadImpactifyer/HeadPublifyer";
 import { BodyImpactifyer } from "./components/BodyImpactifyer/BodyImpactifyer";
+import { Grafica } from "./components/Graph/Graph";
+import jsonData from './DataSetTendencias.json'; 
+
+
 
 const AppUi = () => {
   return (
@@ -9,6 +13,8 @@ const AppUi = () => {
       <HeadPublifyer />
       <MenuPublifyer />
       <BodyImpactifyer />
+      <ChartComponent dataset={jsonData} />
+
     </>
   );
 };
