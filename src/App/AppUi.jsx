a8de602b82dc26a29c821e6ad93681759ebdb510
FranBW <milerolarte@gmail.com>
2023-07-09 02:25:20 -0500

    Verga y Miseria

--- src/App/AppUi.jsx
+++ src/App/AppUi.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { MenuPublifyer } from "./components/MenuPublifyer/MenuPublifyer";
 import { HeadPublifyer } from "./components/HeadImpactifyer/HeadPublifyer";
 import { BodyImpactifyer } from "./components/BodyImpactifyer/BodyImpactifyer";
-import { Grafica } from "./components/Grafica/Graph.js";
+import ChartComponent from './components/ChartComponent/ChartComponent';
+import dataSet from './src/DataSetTendencias.json';
 
 const AppUi = () => {
   return (
@@ -10,8 +11,10 @@ const AppUi = () => {
       <HeadPublifyer />
       <MenuPublifyer />
       <BodyImpactifyer />
+      <ChartComponent dataSet={dataSet} />
     </>
   );
 };
 
 export { AppUi };
+
